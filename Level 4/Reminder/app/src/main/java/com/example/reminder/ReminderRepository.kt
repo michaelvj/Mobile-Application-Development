b77b801b15ffeca03324a3afef68a2d0725d6252
michaelvj <michaelvanjelgerhuis@gmail.com>
2020-04-14 14:52:03 +0200

    Level 4 example

---
+++ Level 4/Reminder/app/src/main/java/com/example/reminder/ReminderRepository.kt
@@ -0,0 +1,29 @@
+package com.example.reminder
+
+import android.content.Context
+
+public class ReminderRepository(context: Context) {
+    private var reminderDao: ReminderDao
+
+    init{
+        val reminderRoomDatabase =
+            ReminderRoomDatabase.getDatabase(context)
+        reminderDao = reminderRoomDatabase!!.reminderDao()
+    }
+
+    suspend fun getAllReminders(): List<Reminder>{
+        return reminderDao.getAllReminders()
+    }
+
+    suspend fun insertReminder(reminder: Reminder){
+        reminderDao.insertReminder(reminder)
+    }
+
+    suspend fun updateReminder(reminder: Reminder){
+        reminderDao.updateReminder(reminder)
+    }
+
+    suspend fun deleteReminder(reminder: Reminder){
+        reminderDao.deleteReminder(reminder)
+    }
+}
