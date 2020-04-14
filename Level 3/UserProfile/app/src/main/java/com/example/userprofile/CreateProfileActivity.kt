088942d9a0872f159d24af80d48f3b58a9aa8c9a
michaelvj <michaelvanjelgerhuis@gmail.com>
2020-04-14 13:05:56 +0200

    Level 3 Task 1

---
+++ Level 3/UserProfile/app/src/main/java/com/example/userprofile/CreateProfileActivity.kt
@@ -0,0 +1,63 @@
+package com.example.userprofile
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.userprofile.models.Profile
+import kotlinx.android.synthetic.main.activity_create_profile.*
+
+const val GALLERY_REQUEST_CODE = 100
+
+class CreateProfileActivity : AppCompatActivity() {
+    private var profileImageUri: Uri? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_profile)
+
+        initViews()
+    }
+
+    private fun initViews(){
+        btnOpenGallery.setOnClickListener{ onGalleryClick() }
+        btnConfirm.setOnClickListener { onConfirmClick() }
+    }
+
+    private fun onGalleryClick() {
+        val galleryIntent = Intent(Intent.ACTION_PICK)
+
+        galleryIntent.type = "image/*"
+        startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE)
+
+    }
+
+    private fun onConfirmClick(){
+        val profile = Profile(
+            etFirstName.text.toString(),
+            etLastName.text.toString(),
+            etDescription.text.toString(),
+            profileImageUri
+        )
+
+        val profileActivityIntent = Intent(this,
+        ProfileActivity::class.java)
+        profileActivityIntent.putExtra(ProfileActivity.PROFILE_EXTRA, profile)
+        startActivity(profileActivityIntent)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK){
+            when(requestCode) {
+                GALLERY_REQUEST_CODE -> {
+                    profileImageUri = data?.data
+                    ivProfileImage.setImageURI(profileImageUri)
+                }
+            }
+        }
+    }
+
+
+}
